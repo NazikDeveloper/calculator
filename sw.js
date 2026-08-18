8fe0db54cb08bcb3ccd747ebd997d839d6082fbf
Nazik <nazikfrontenddeveloper@gmail.com>
2026-08-18 19:32:03 +0300

    сщм

--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
-const CACHE_NAME = "price-calculator-v1";
+const CACHE_NAME = "price-calculator-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./css/style.css",
+  "./manifest.json",
+  "./image/icon-192.png",
+  "./image/icon-512.png",
+  "./fonts/poppins-v24-latin-regular.woff2",
+  "./fonts/poppins-v24-latin-500.woff2",
+  "./fonts/poppins-v24-latin-600.woff2",
+  "./fonts/poppins-v24-latin-800.woff2",
+  "./fonts/poppins-v24-latin-900.woff2",
 ];
 
 self.addEventListener("install", event => {
@@ -21,3 +29,49 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const CACHE_NAME = "price-calculator-v2";
+
+// const FILES_TO_CACHE = [
+//   "./",
+//   "./index.html",
+//   "./css/style.css",
+// ];
+
+// self.addEventListener("install", event => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then(cache => {
+//       return cache.addAll(FILES_TO_CACHE);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", event => {
+//   event.respondWith(
+//     caches.match(event.request).then(response => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
